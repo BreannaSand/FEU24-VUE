a1e3ae47c526dd90af83af2f7be07db5451eee9f
Breanna Sand <breanna.sand8@gmail.com>
2025-02-04 11:30:31 +0100

    Created env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
+const apiKey = import.meta.env.API_KEY
 
-const API_KEY = '95703c837094a0b9b9702093a2dc1c48'
-
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${ApiKey}&language=en-US&page=1`
 
 
 fetch(API_URL)
